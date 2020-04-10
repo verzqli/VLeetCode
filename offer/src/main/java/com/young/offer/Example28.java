310571ef05d592498c8e2ddbec7fcc6be71a95cb
xupei <710275215@qq.com>
2020-04-10 18:40:18 +0800

    理解28题原理

--- offer/src/main/java/com/young/offer/Example28.java
+++ offer/src/main/java/com/young/offer/Example28.java
@@ -1,44 +1,46 @@
 package com.young.offer;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.HashSet;
-import java.util.Set;
 
 public class Example28 {
     StringBuilder builder = new StringBuilder();
-    HashSet   set=new HashSet();;
+    char[] chars;
+
     public static void main(String[] args) {
-        new Example28().solve("abcd");
+        new Example28().solve("112");
     }
 
     public void solve(String s) {
-        char[] chars = s.toCharArray();
-        dfs(chars,0);
+        chars = s.toCharArray();
+        dfs(0);
 
     }
 
-    private void dfs(char[] chars,int i ) {
-        if (i==chars.length-1){
+    private void dfs(int index) {
+        if (index == chars.length - 1) {
             System.out.println(Arrays.toString(chars));
             return;
         }
-        set=new HashSet();
-        for (int j = i; j < chars.length; j++) {
-            if (set.contains(chars[i])){
+        //这里有两种方式剪枝去重，一种是在最后对输出的字符串去重，
+        //一种是这里对字符串里单个字符去重，这种方式可以减少运算循环。效果更好
+        HashSet<Character> set = new HashSet<>();
+        for (int i = index; i < chars.length; i++) {
+            if (set.contains(chars[i])) {
                 continue;
             }
-            set.add(chars[i]);
-            swap(chars,i,j);
-            dfs(chars,i+1);
-            swap(chars,i,j);
+            //交换后需要交换回来，当前交换不能对下一个交换产生影响
+            set.add(chars[index]);
+            swap(chars, index, i);
+            dfs(index + 1);
+            swap(chars, index, i);
         }
     }
 
     private void swap(char[] chars, int i, int j) {
         char temp = chars[i];
-        chars[i]=chars[j];
-        chars[j]=temp;
+        chars[i] = chars[j];
+        chars[j] = temp;
     }
 
 }
